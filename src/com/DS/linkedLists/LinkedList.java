a53b9c746683c3c0f357a3d080829ea33acb9c10
Pankaj Garg <garg.pankaj15@gmail.com>
2018-07-01 20:11:01 +0530

    Linked Lists Implementation

---
+++ src/com/DS/linkedLists/LinkedList.java
@@ -0,0 +1,109 @@
+package com.DS.linkedLists;
+
+public class LinkedList {
+	Node head;
+
+	static class Node {
+		int data;
+
+		Node next;
+
+		Node(int inputData) {
+			this.data = inputData;
+			this.next = null;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Started");
+
+		LinkedList ll = new LinkedList();
+
+		Node FirstNode = new Node(1);
+		Node SecondNode = new Node(2);
+		Node ThirdNode = new Node(3);
+
+		ll.head = FirstNode;
+		ll.head.next = SecondNode;
+		ll.head.next.next = ThirdNode;
+
+		ll.printList();
+
+		Node Forth = new Node(4);
+		ll = insertToListOnFront(ll, Forth);
+		System.out.println("Insert 4 on head..");
+		ll.printList();
+
+		Node Fifth = new Node(5);
+		ll = insertAfterGivenNode(ll, Forth, Fifth);
+		System.out.println("Insert 5 after 4..");
+		ll.printList();
+
+		Node Sixtth = new Node(6);
+		ll = insertAtTheEnd(ll, Sixtth);
+		System.out.println("Insert 6 at the end");
+		ll.printList();
+
+		ll.deleteKeyFromList(6);
+		ll.printList();
+	}
+
+	void deleteKeyFromList(int key) {
+		Node Previous = null;
+		Node Current = this.head;
+
+		// If the head node itself holds the key to be deleted..
+		if (this.head.data == key) {
+			this.head = this.head.next;
+			return;
+		}
+
+		while (Current != null) {
+			if (Current.data == key) {
+				Previous.next = Current.next;
+				break;
+			}
+
+			Previous = Current;
+			Current = Current.next;
+		}
+	}
+
+	static LinkedList insertToListOnFront(LinkedList ll, Node node) {
+		node.next = ll.head;
+		ll.head = node;
+
+		return ll;
+	}
+
+	static LinkedList insertAfterGivenNode(LinkedList ll, Node PreviousNode, Node NewNode) {
+		NewNode.next = PreviousNode.next;
+
+		PreviousNode.next = NewNode;
+
+		return ll;
+	}
+
+	static LinkedList insertAtTheEnd(LinkedList ll, Node NewNode) {
+		Node LastNode = ll.head;
+		while (LastNode.next != null) {
+			LastNode = LastNode.next;
+		}
+
+		LastNode.next = NewNode;
+
+		return ll;
+	}
+
+	void printList() {
+		Node next = this.head;
+
+		while (next != null) {
+			System.out.println(next.data);
+
+			next = next.next;
+		}
+
+		System.out.println("END \n");
+	}
+}
---
+++ src/com/DS/linkedLists/Node.java
@@ -0,0 +1,5 @@
+package com.DS.linkedLists;
+
+public class Node {
+
+}
